a3250e9528a06bc1f14ef99682b6b1bbe30703ae
bdream <bdletterbox@gmail.com>
2023-05-06 20:37:55 +0300

    Delete all cycles except winner. Allow move cycles after touch.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let players = [];
+const circles = {};
 
-const container = document.getElementById('container');
-const playerCounter = document.getElementById('player-counter');
-container.addEventListener('touchstart', handleTouchStart);
-container.addEventListener('touchend', handleTouchEnd);
+container.addEventListener('touchmove', handleTouchMove);
 
 function handleTouchStart(event) {
     if (players.length === 0) {
@@ -13,8 +10,9 @@ function handleTouchStart(event) {
             }
         }, 5000);
     }
-    players.push(event.changedTouches[0].identifier);
-    drawCircle(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+    const touchId = event.changedTouches[0].identifier;
+    players.push(touchId);
+    drawCircle(touchId, event.changedTouches[0].clientX, event.changedTouches[0].clientY);
     updatePlayerCounter();
 }
 
@@ -27,7 +25,7 @@ function handleTouchEnd(event) {
     updatePlayerCounter();
 }
 
-function drawCircle(x, y) {
+function drawCircle(id, x, y) {
     const circle = document.createElement('div');
     circle.style.width = '100px';
     circle.style.height = '100px';
@@ -35,32 +33,33 @@ function drawCircle(x, y) {
     circle.style.left = `${x - 50}px`;
     circle.style.top = `${y - 50}px`;
     circle.classList.add('circle');
+    circle.dataset.touchId = id;
     container.appendChild(circle);
+    circles[id] = circle;
 }
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function updatePlayerCounter() {
-    playerCounter.innerText = `Игроков: ${players.length}`;
+function handleTouchMove(event) {
+    event.preventDefault();
+    const touch = event.changedTouches[0];
+    const circle = circles[touch.identifier];
+    if (!circle) return;
+    circle.style.left = `${touch.clientX - 50}px`;
+    circle.style.top = `${touch.clientY - 50}px`;
 }
 
 function determineRandomPlayer() {
     const randomPlayer = players[Math.floor(Math.random() * players.length)];
     players = [];
-    displayRandomPlayer(randomPlayer);
+    removeLosingCircles(randomPlayer);
     updatePlayerCounter();
 }
 
-function displayRandomPlayer(randomPlayer) {
-    container.innerHTML = `<h1>Случайный игрок: ${randomPlayer}</h1>`;
-    setTimeout(() => {
-        container.innerHTML = '';
-    }, 5000);
+function removeLosingCircles(winnerId) {
+    Object.keys(circles).forEach(id => {
+        if (id !== winnerId.toString()) {
+            container.removeChild(circles[id]);
+            delete circles[id];
+        }
+    });
 }
+
